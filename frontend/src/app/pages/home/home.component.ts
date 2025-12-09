25385c50e01286979780fc4ddeb262a8ed0d2fec
troversones <kisbencerobert2002@gmail.com>
2025-12-09 01:01:05 +0100

    Style changes to frontend: Added hungarian text, changed style/text list on home

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Shield, TriangleAlert, Lock, Code } from 'lucide-angular';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+  readonly Shield = Shield;
+  readonly TriangleAlert = TriangleAlert;
+  readonly Lock = Lock;
+  readonly Code = Code;
+}
